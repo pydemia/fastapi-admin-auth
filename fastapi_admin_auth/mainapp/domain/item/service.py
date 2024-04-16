2aaacfa8cb936c7f7bdd4312cce38736fb8cf065
Youngju Kim <yj.kim1@sk.com>
2024-04-16 20:34:58 +0900

    add service

--- fastapi_admin_auth/mainapp/domain/item/service.py
+++ fastapi_admin_auth/mainapp/domain/item/service.py
@@ -1,4 +1,21 @@
 from .models import Item
+from .crud import (
+    get_items_all,
+    get_items_by_ids,
+    get_items_by_range,
+    get_item_by_id,
+    create_item,
+    update_item,
+    delete_item,
+)
 
+get_items_all
+get_items_by_ids
+get_items_by_range
+get_item_by_id
+create_item
+update_item
+delete_item
 def get_item_by_id(item_id: int) -> Item:
     return Item(id=item_id)
+
