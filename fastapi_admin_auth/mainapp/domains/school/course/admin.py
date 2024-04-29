8d9ca075e68be90deb0b34a495beb1408e974cdb
Youngju Kim <yj.kim1@sk.com>
2024-04-29 15:29:49 +0900

    add a case: many-to-many

--- fastapi_admin_auth/mainapp/domains/school/course/admin.py
+++ fastapi_admin_auth/mainapp/domains/school/course/admin.py
@@ -25,7 +25,7 @@ from starlette_admin import (
     # ArrowField,
 # #     ColorField,
     HasOne,
-    # HasMany,
+    HasMany,
 )
 
 # from mainapp.core.admin import AuthorizedModelView
@@ -64,6 +64,11 @@ class CourseModelView(ModelView):
         HasOne("book", identity="textbook"),
         HasOne("certificate", identity="certificate", required=True),
         HasOne("teacher", identity="teacher", required=True),
+        HasMany("students", identity="student", required=False)
+        # HasOne(Course.book.key, identity="textbook"),
+        # HasOne(Course.certificate.key, identity="certificate", required=True),
+        # HasOne(Course.teacher.key, identity="teacher", required=True),
+        # HasMany(Course.students.key, identity="Student[multiple, optional]", required=False)
         # TagsField("tags", label="Tags"),
         # CollectionField(
         #     "certificate",
--- fastapi_admin_auth/mainapp/domains/school/student/models.py
+++ fastapi_admin_auth/mainapp/domains/school/student/models.py
@@ -4,7 +4,12 @@ __all__ = [
     "Student",
 ]
 
-from sqlmodel import Field, SQLModel
+from sqlmodel import Field, SQLModel, Relationship
+from typing import TYPE_CHECKING
+
+from ..course.models import CourseStudentLink
+if TYPE_CHECKING:
+    from ..course.models import Course
 
 
 class Student(SQLModel, table=True):
@@ -12,3 +17,8 @@ class Student(SQLModel, table=True):
     firstname: str
     lastname: str
     description: str = Field("")
+
+    courses: list["Course"] = Relationship(
+        back_populates="students",
+        link_model=CourseStudentLink,
+    )
